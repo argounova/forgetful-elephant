70d308781d4b0987863a47b2382267ef90c8b622
Volantis <putzstuck@gmail.com>
2023-01-11 10:14:59 -0500

    imported react bootstrap and established foundation for home

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import '../../Assets/Styles/home.css';
+
+function Home() {
+    return(
+        <Container fluid>
+            <Container>
+                <Row>
+                    <Col>1 of 2</Col>
+                    <Col>2 of 2</Col>
+                </Row>
+            </Container>
+        </Container>
+    );
+}
+
+export default Home;
