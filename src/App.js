e5d25147a648dfb60ac19629e2004855c2c70060
Volantis <putzstuck@gmail.com>
2023-01-11 15:52:50 -0500

    added react bootstrap elements to home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/index';
-import Navigation from './Components/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route
-    //       path='/'
-    //       element={Home}
-    //     />
-    //   </Routes>
-    // </Router>
-    // <Navigation/>
     <Home />
   );
 }
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 import '../../Assets/Styles/home.css';
 import Navigation from '../../Components/Navigation';
 
@@ -14,8 +16,23 @@ function Home() {
             <Container fluid>
                 <Container>
                     <Row>
-                        <Col>1 of 2</Col>
-                        <Col>2 of 2</Col>
+                        <Col xs={12} md={8}>
+                            <Stack gap={2}>
+                                <h3>Start a new note</h3>
+                                <input placeholder='Note title'></input>
+                                <textarea></textarea>
+                                <Button variant='primary'>Save Note</Button>
+                            </Stack>
+                        </Col>
+                        <Col xs={12} md={4}>
+                            <Stack gap={1}>
+                                <h3>Saved Notes</h3>
+                                <Button variant='primary'>Note 1</Button>
+                                <Button variant='primary'>Note 2</Button>
+                                <Button variant='primary'>Note 3</Button>
+                                <Button variant='primary'>Note 4</Button>
+                            </Stack>
+                        </Col>
                     </Row>
                 </Container>
             </Container>
