0bbcf53ea44de2a90f865919a5d7ff4d512768c0
Volantis <putzstuck@gmail.com>
2023-01-11 16:04:20 -0500

    added react bootstrap elements to note page and created css file

---
+++ src/Pages/Note/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import '../../Assets/Styles/note.css';
+import Navigation from '../../Components/Navigation';
+
+function Note() {
+    return(
+        <>
+            <header>
+                <Navigation/>
+            </header>
+            <Container fluid>
+                <Container>
+                    <Row>
+                        <Stack gap={2}>
+                            <h3>Note 1</h3>
+                            <input placeholder='Note title'></input>
+                            <textarea></textarea>
+                        </Stack>
+                    </Row>
+                    <Row>
+                        <Stack direction='horizontal' gap={2}>
+                            <Button variant='primary'>Save</Button>
+                            <div className='vr'/>
+                            <Button variant='dark'>Cancel</Button>
+                        </Stack>
+                    </Row>
+                </Container>
+            </Container>
+        </>
+    );
+}
+
+export default Note;
